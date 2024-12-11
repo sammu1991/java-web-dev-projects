72637f80621f120c2ceba671499a1b2efe7e1a3c
samatakarka <samata.karka@gmail.com>
2024-12-11 14:13:24 -0600

    first project

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -3,18 +3,32 @@ import java.util.Scanner;
 
 public class Area {
     public static void main(String[] args) {
-       // double a;
-        Scanner input;
 
-        input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        double radius;
         System.out.println("Enter a radius: ");
-        double radius = input.nextDouble();
-        input.close();
-        double a =  Circle.getArea(radius) ;
+        do {
+            if (input.hasNextDouble()) {
+                 radius= input.nextDouble();
+                if (radius > 0) {
+                    break;
+                } else {
+                    System.out.println("Number must be positive. Enter a valid number: ");
+                }
+            } else {
+                System.out.println("Not a number. Enter a valid number: ");
+                input.next();
+            }
+        } while (true);
+
+
+        double a = Circle.getArea(radius);
         System.out.println("Area of Circle is: " + a);
+
+    }
 
 
     }
-}
+
 
 
--- datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Circle.java
@@ -1,7 +1,6 @@
 package org.launchcode;
 
 public class Circle {
-
     public static double getArea(double radius) {
         return 3.14 * radius * radius;
     }
