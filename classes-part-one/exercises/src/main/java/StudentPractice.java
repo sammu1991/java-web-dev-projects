f9db88134df80aa405a1a5f58c6c15098360abf2
samatakarka <samata.karka@gmail.com>
2024-12-13 19:09:03 -0600

    Exercises done.

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,8 @@
 public class StudentPractice {
     public static void main(String[] args){
         //insantiate your Student class below
+
+        Student student = new Student("Samata Karka", 125, 1, 4.0);
+        System.out.println(student.getNumberOfCredits());
     }
 }
---
+++ classes-part-one/exercises/src/main/java/Teacher.java
@@ -0,0 +1,40 @@
+public class Teacher {
+
+
+    private String firstName;
+    private String lastname;
+    private String subject;
+    private Integer yearsTeaching;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Integer getYearsTeaching() {
+        return yearsTeaching;
+    }
+
+    public void setYearsTeaching(Integer yearsTeaching) {
+        this.yearsTeaching = yearsTeaching;
+    }
+}
